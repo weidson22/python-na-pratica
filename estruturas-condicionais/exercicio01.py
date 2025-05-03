9f69860cd5e48a0675c4d18c4887edd21ecba277
weidson22 <weidsoniarley86@gmail.com>
2025-05-03 19:20:58 -0300

    Create exercicio01.py

---
+++ estruturas-condicionais/exercicio01.py
@@ -0,0 +1,9 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite mais um número: '))
+if n1 > n2:
+    print(f'O número maior é {n1}')
+elif n2 > n1:
+    print(f'O número maior é {n2}')
+else:
+    print(f'Os número têm o mesmo valor!')
+  
