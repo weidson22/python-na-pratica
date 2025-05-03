0c41808161cca9020c9f5f61056c2e8ec5881d98
weidson22 <weidsoniarley86@gmail.com>
2025-05-03 19:23:38 -0300

    Create exercicio02.py

---
+++ estruturas-condicionais/exercicio02.py
@@ -0,0 +1,8 @@
+#verificando se o número é positivo ou negativo
+n = float(input('Digite um número: '))
+if n > 0:
+    print('Esse número é positivo')
+elif n < 0:
+    print('Esse número é negativo')
+else:
+    print('O número digitado é 0')
