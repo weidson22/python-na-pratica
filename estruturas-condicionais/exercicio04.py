7bc168ebbc28102f628cfe33413f146139e2885c
weidson22 <weidsoniarley86@gmail.com>
2025-05-03 19:26:34 -0300

    Create exercicio04.py

---
+++ estruturas-condicionais/exercicio04.py
@@ -0,0 +1,14 @@
+#verificando se letra é vogal ou consoante
+letra = str(input('Digite uma letra: ')).strip().title()
+if letra == 'A':
+    print('A letra que você digitou é uma vogal')
+elif letra == 'E':
+    print('A letra que você digitou é uma vogal')
+elif letra == 'I':
+    print('A letra que você digitou é uma vogal')
+elif letra == 'O':
+    print('A letra que você digitou é uma vogal')
+elif letra == 'U':
+    print('A letra que você digitou é uma vogal')
+else:
+    print('A letra que você digitou é uma consoante')
